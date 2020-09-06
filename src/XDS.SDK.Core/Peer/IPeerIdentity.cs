4f892b0127f18433bcc15dc159e3c541f038a928
soma42 <soma42@protonmail.ch>
2020-09-06 19:12:24 +0200

    peeridentity

---
+++ src/XDS.SDK.Core/Peer/IPeerIdentity.cs
@@ -0,0 +1,15 @@
+﻿namespace XDS.SDK.Core.Peer
+{
+    public interface IPeerIdentity
+    {
+        PeerIdentityType PeerIdentityType { get; set; }
+
+        byte[] PublicKey { get; set; }
+
+        byte[] PublicKeyHash { get; set; }
+
+        string ShortId { get; set; }
+    }
+}
+
+
---
+++ src/XDS.SDK.Core/Peer/PeerIdentityType.cs
@@ -0,0 +1,8 @@
+﻿namespace XDS.SDK.Core.Peer
+{
+    public enum PeerIdentityType : int
+    {
+        NotSet = 0,
+        Default = 1
+    }
+}
